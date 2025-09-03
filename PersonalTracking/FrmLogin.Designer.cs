30bc05e87494690cd08afb7b2a5a244b01518e49
EdisonDotBit <junior.lati@gmail.com>
2025-09-03 17:15:47 +0800

    Rename header in FrmLogin

--- PersonalTracking/FrmLogin.Designer.cs
+++ PersonalTracking/FrmLogin.Designer.cs
@@ -62,9 +62,9 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(150, 32);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(291, 29);
+            this.label1.Size = new System.Drawing.Size(281, 29);
             this.label1.TabIndex = 0;
-            this.label1.Text = "EMPLOYEE TRACKING";
+            this.label1.Text = "EMPLOYEE TRACKER";
             // 
             // lblPassword
             // 
