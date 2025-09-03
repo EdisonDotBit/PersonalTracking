cea87316fffd3261b92ce1078f192d7071ba5bdf
EdisonDotBit <junior.lati@gmail.com>
2025-09-03 16:30:50 +0800

    Create FrmPermission

---
+++ PersonalTracking/FrmPermission.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace PersonalTracking
+{
+    partial class FrmPermission
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUserNo = new System.Windows.Forms.Label();
+            this.txtUserNo = new System.Windows.Forms.TextBox();
+            this.dpEnd = new System.Windows.Forms.DateTimePicker();
+            this.dpStart = new System.Windows.Forms.DateTimePicker();
+            this.lblFinishDate = new System.Windows.Forms.Label();
+            this.lblStartDate = new System.Windows.Forms.Label();
+            this.lblDayAmount = new System.Windows.Forms.Label();
+            this.txtDayAmount = new System.Windows.Forms.TextBox();
+            this.lblExplanation = new System.Windows.Forms.Label();
+            this.txtExplanation = new System.Windows.Forms.TextBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblUserNo
+            // 
+            this.lblUserNo.AutoSize = true;
+            this.lblUserNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserNo.Location = new System.Drawing.Point(17, 26);
+            this.lblUserNo.Name = "lblUserNo";
+            this.lblUserNo.Size = new System.Drawing.Size(119, 20);
+            this.lblUserNo.TabIndex = 0;
+            this.lblUserNo.Text = "User Number:";
+            // 
+            // txtUserNo
+            // 
+            this.txtUserNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUserNo.Location = new System.Drawing.Point(142, 23);
+            this.txtUserNo.Name = "txtUserNo";
+            this.txtUserNo.ReadOnly = true;
+            this.txtUserNo.Size = new System.Drawing.Size(227, 26);
+            this.txtUserNo.TabIndex = 1;
+            // 
+            // dpEnd
+            // 
+            this.dpEnd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dpEnd.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dpEnd.Location = new System.Drawing.Point(142, 108);
+            this.dpEnd.Name = "dpEnd";
+            this.dpEnd.Size = new System.Drawing.Size(227, 26);
+            this.dpEnd.TabIndex = 5;
+            // 
+            // dpStart
+            // 
+            this.dpStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dpStart.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dpStart.Location = new System.Drawing.Point(142, 66);
+            this.dpStart.Name = "dpStart";
+            this.dpStart.Size = new System.Drawing.Size(227, 26);
+            this.dpStart.TabIndex = 3;
+            // 
+            // lblFinishDate
+            // 
+            this.lblFinishDate.AutoSize = true;
+            this.lblFinishDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFinishDate.Location = new System.Drawing.Point(17, 113);
+            this.lblFinishDate.Name = "lblFinishDate";
+            this.lblFinishDate.Size = new System.Drawing.Size(106, 20);
+            this.lblFinishDate.TabIndex = 4;
+            this.lblFinishDate.Text = "Finish Date:";
+            // 
+            // lblStartDate
+            // 
+            this.lblStartDate.AutoSize = true;
+            this.lblStartDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStartDate.Location = new System.Drawing.Point(17, 72);
+            this.lblStartDate.Name = "lblStartDate";
+            this.lblStartDate.Size = new System.Drawing.Size(98, 20);
+            this.lblStartDate.TabIndex = 2;
+            this.lblStartDate.Text = "Start Date:";
+            // 
+            // lblDayAmount
+            // 
+            this.lblDayAmount.AutoSize = true;
+            this.lblDayAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDayAmount.Location = new System.Drawing.Point(17, 154);
+            this.lblDayAmount.Name = "lblDayAmount";
+            this.lblDayAmount.Size = new System.Drawing.Size(112, 20);
+            this.lblDayAmount.TabIndex = 6;
+            this.lblDayAmount.Text = "Day Amount:";
+            // 
+            // txtDayAmount
+            // 
+            this.txtDayAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDayAmount.Location = new System.Drawing.Point(142, 151);
+            this.txtDayAmount.Name = "txtDayAmount";
+            this.txtDayAmount.ReadOnly = true;
+            this.txtDayAmount.Size = new System.Drawing.Size(227, 26);
+            this.txtDayAmount.TabIndex = 7;
+            // 
+            // lblExplanation
+            // 
+            this.lblExplanation.AutoSize = true;
+            this.lblExplanation.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblExplanation.Location = new System.Drawing.Point(17, 199);
+            this.lblExplanation.Name = "lblExplanation";
+            this.lblExplanation.Size = new System.Drawing.Size(108, 20);
+            this.lblExplanation.TabIndex = 8;
+            this.lblExplanation.Text = "Explanation:";
+            // 
+            // txtExplanation
+            // 
+            this.txtExplanation.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtExplanation.Location = new System.Drawing.Point(142, 196);
+            this.txtExplanation.Multiline = true;
+            this.txtExplanation.Name = "txtExplanation";
+            this.txtExplanation.Size = new System.Drawing.Size(406, 191);
+            this.txtExplanation.TabIndex = 9;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(297, 437);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(162, 50);
+            this.btnClose.TabIndex = 11;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(110, 437);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(162, 50);
+            this.btnSave.TabIndex = 10;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // FrmPermission
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 522);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.lblExplanation);
+            this.Controls.Add(this.txtExplanation);
+            this.Controls.Add(this.lblDayAmount);
+            this.Controls.Add(this.txtDayAmount);
+            this.Controls.Add(this.dpEnd);
+            this.Controls.Add(this.dpStart);
+            this.Controls.Add(this.lblFinishDate);
+            this.Controls.Add(this.lblStartDate);
+            this.Controls.Add(this.lblUserNo);
+            this.Controls.Add(this.txtUserNo);
+            this.Name = "FrmPermission";
+            this.Text = "Employee Tracker - Permission";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUserNo;
+        private System.Windows.Forms.TextBox txtUserNo;
+        private System.Windows.Forms.DateTimePicker dpEnd;
+        private System.Windows.Forms.DateTimePicker dpStart;
+        private System.Windows.Forms.Label lblFinishDate;
+        private System.Windows.Forms.Label lblStartDate;
+        private System.Windows.Forms.Label lblDayAmount;
+        private System.Windows.Forms.TextBox txtDayAmount;
+        private System.Windows.Forms.Label lblExplanation;
+        private System.Windows.Forms.TextBox txtExplanation;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
