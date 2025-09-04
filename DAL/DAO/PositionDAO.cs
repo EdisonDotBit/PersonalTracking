20da78b5e942a0ec0e5e1b0244c62caff788d8cc
EdisonDotBit <junior.lati@gmail.com>
2025-09-04 15:58:32 +0800

    Create PositionDAO and PositionDTO

---
+++ DAL/DAO/PositionDAO.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.DTO;
+
+namespace DAL.DAO
+{
+    public class PositionDAO : EmployeeContext
+    {
+        public static void AddPosition(POSITION position)
+        {
+            try
+            {
+                db.POSITIONs.InsertOnSubmit(position);
+                db.SubmitChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static List<PositionDTO> GetPositions()
+        {
+            try
+            {
+                var positionList = (from p in db.POSITIONs
+                            join d in db.DEPARTMENTs
+                            on p.DepartmentID equals d.ID
+                            select new PositionDTO
+                            {
+                                ID = p.ID,
+                                PositionName = p.PositionName,
+                                DepartmentName = d.DepartmentName,
+                                DepartmentID = p.DepartmentID
+
+                            }).OrderBy(x => x.ID).ToList();
+                return positionList;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
