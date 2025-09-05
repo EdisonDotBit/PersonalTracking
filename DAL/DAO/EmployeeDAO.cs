bd61b286f944901f751419fc496d7f6f5b1c62c6
EdisonDotBit <junior.lati@gmail.com>
2025-09-05 10:07:02 +0800

    Add GetEmployees method in EmployeeDAO.cs

--- DAL/DAO/EmployeeDAO.cs
+++ DAL/DAO/EmployeeDAO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DAL.DTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,6 +20,33 @@ namespace DAL.DAO
             {
                 throw ex;
             }
+        }
+
+        public static List<EmployeeDetailDTO> GetEmployees()
+        {
+            List<EmployeeDetailDTO> employeeList = new List<EmployeeDetailDTO>();
+
+            return (from e in db.EMPLOYEEs
+                        join d in db.DEPARTMENTs on e.DepartmentID equals d.ID
+                        join p in db.POSITIONs on e.PositionID equals p.ID
+                        select new EmployeeDetailDTO
+                        {
+                            UserNo = e.UserNo,
+                            Name = e.Name,
+                            Surname = e.Surname,
+                            EmployeeID = e.ID,
+                            Password = e.Password,
+                            DepartmentName = d.DepartmentName,
+                            PositionName = p.PositionName,
+                            DepartmentID = e.DepartmentID,
+                            PositionID = e.PositionID,
+                            isAdmin = e.isAdmin,
+                            Salary = e.Salary,
+                            ImagePath = e.ImagePath,
+                            BirthDay = e.BirthDay,
+                            Address = e.Address
+                        }).ToList();
+       
         }
 
         public static List<EMPLOYEE> GetUsers(int v)
