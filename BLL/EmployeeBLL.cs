081355f0e7af85744574a0b142875a4f6e8eb659
EdisonDotBit <junior.lati@gmail.com>
2025-09-05 08:57:32 +0800

    Create EmployeBL

---
+++ BLL/EmployeeBLL.cs
@@ -0,0 +1,39 @@
+﻿using DAL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.DAO;
+using DAL;
+
+namespace BLL
+{
+    public class EmployeeBLL
+    {
+        public static void AddEmployee(EMPLOYEE employee)
+        {
+            EmployeeDAO.AddEmployee(employee); 
+        }
+
+        public static EmployeeDTO GetAll()
+        {
+            EmployeeDTO employeeDTO = new EmployeeDTO();
+            employeeDTO.Departments = DepartmentDAO.GetDepartments();
+            employeeDTO.Positions = PositionDAO.GetPositions();
+            return employeeDTO;
+        }
+
+        public static bool isUnique(int v)
+        {
+            List<EMPLOYEE> employeeList = EmployeeDAO.GetUsers(v);
+            if(employeeList.Count > 0)
+            {
+                return false;
+            } else
+            {
+                return true;
+            }
+        }
+    }
+}
